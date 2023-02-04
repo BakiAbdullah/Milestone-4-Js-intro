02e94df3889e23825479cbf13eb2799d224f8609
BakiAbdullah <shatil605@gmail.com>
2023-02-04 23:54:14 +0600

    Solved some problems FH

--- Module 23.5 Practice js problems/problem-solving_checklist.js
+++ Module 23.5 Practice js problems/problem-solving_checklist.js
@@ -1,35 +1,66 @@
-function feetToInch(feet) {
-  let inch = feet * 12;
-  return inch;
-}
-let convertedInch = feetToInch(2);
+// function feetToInch(feet) {
+//   let inch = feet * 12;
+//   return inch;
+// }
+// let convertedInch = feetToInch(2);
 // console.log(convertedInch);
 
 
-function centimeterToMeter(centimeter) {
-  let meter = centimeter / 100;
-  return meter;
-}
-let totalMeter = centimeterToMeter(400);
+// function centimeterToMeter(centimeter) {
+//   let meter = centimeter / 100;
+//   return meter;
+// }
+// let totalMeter = centimeterToMeter(400);
 // console.log(totalMeter);
 
 
 //? 3.=====================================
-function paperRequirements(firstBook, secondBook, thirdBook) {
-  
-}
+// function paperRequirements(firstBook, secondBook, thirdBook) {
+//   let firstBookCost = firstBook * 100;
+//   let secondBookCost = secondBook * 200;
+//   let thirdBookCost = thirdBook * 300;
 
+//   const totalBookCost = firstBookCost + secondBookCost + thirdBookCost;
+//   return totalBookCost;
+// }
 
+// let totalBooks = paperRequirements(5, 5, 5);
+// console.log(totalBooks);
 
 //? 4.=====================================
 function bestFriend(arrElements) {
-  for(let i = 0; i< arrElements.length; i++) {
-    
+  let longestFriend = arrElements[0];
+  for(let i = 0; i < arrElements.length; i++) {
+    let friendsElement = friends[i];
+
+    if (friendsElement.length > longestFriend.length) {
+      longestFriend = friendsElement;
+    }
   }
+  return longestFriend;
 }
 
 let friends = ['Kafi', 'Liza', 'Bristy', 'Umme Ammara', 'Hisham'];
-
 let longestFriendName = bestFriend(friends);
 console.log(longestFriendName);
+
+//?
+
+function onlyPositive(numbersArray) {
+  const positiveArr = [];
+  for (let i = 0; i < numbersArray.length; i++) {
+    let arrELement = numbersArray[i];
+    if (arrELement >= 0) {
+      positiveArr.push(arrELement);
+    } else {
+      break;
+    }
+  }
+  return positiveArr;
+  
+}
+
+ let numbersArray = [12, 13, 96, 19, 32, 24, -45, 31];
+ let positiveNum = onlyPositive(numbersArray);
+//  console.log(positiveNum);
 
